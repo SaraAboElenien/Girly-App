e30b52cdcea8184290201a2ce4ee5e62cf4a9460
SaraAboElenien <minalona383@gmail.com>
2025-06-15 17:22:45 +0300

    feat: add Vercel deployment URLs to CORS configuration

--- server/src/initApp.js
+++ server/src/initApp.js
@@ -13,7 +13,9 @@ export const initApp = (app, express) => {
 
   const allowedOrigins = [
     'http://localhost:5173', 
-    'http://localhost:3000', 
+    'http://localhost:3000',
+    'https://girly-app-sara-aboeleniens-projects.vercel.app',
+    'https://girly-app.vercel.app'
   ];
   app.use(cors({
     origin: function(origin, callback) {
